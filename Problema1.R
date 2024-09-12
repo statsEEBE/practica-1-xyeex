efcfef704c96573a6c07b1fab8f1bd95e6b79a8a
wangjiahui.ye <wangjiahui.ye@auleseebe.upc.edu>
2024-09-12 11:45:36 +0200

    mmmmm

--- Problema1.R
+++ Problema1.R
@@ -1,6 +1,42 @@
 #Codigo para problema 1
-#abcdefg
 
-plot(1:5,col='green',pch=16)
+mis_dades <- mtcars;
+
+mean(mis_dades$qsec)
 
 
+hist(mis_dades$qsec)
+
+table(cut(mis_dades$qsec,9))
+
+table(cut(mis_dades$qsec,9))/32
+
+hist(mis_dades$qsec,breaks = 15)
+
+freq <- table(cut(mis_dades$qsec,9)) 
+tabla <- data.frame(ni=freq,
+                    fi=freq/32,
+                    Ni=cumsum(freq),
+                    Fi=cumsum(freq/32))
+tabla
+
+mis_dades$drat
+hist(mis_dades$drat)
+median(mis_dades$drat)
+sort(mis_dades$drat)
+quantile(mis_dades$drat)
+
+quantile(mis_dades$mpg)
+sort(mis_dades$mpg)
+quantile(mis_dades$mpg)
+
+quantile(mis_dades$mpg,0.18)
+
+
+mis_dades$cyl
+IQR(mis_dades$cyl)
+sort(mis_dades$cyl)
+quantile(mis_dades$cyl)
+
+sd(mis_dades$cyl)
+var(mis_dades$cyl)
